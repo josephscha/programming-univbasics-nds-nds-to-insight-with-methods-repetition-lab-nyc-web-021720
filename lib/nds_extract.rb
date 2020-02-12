7d1376276280182880a4c8e23057c73111a1910c
Joseph S. Cha <josephshincha@gmail.com>
2020-02-12 23:00:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,7 +50,7 @@ def total_gross(source)
   index = 0 
   total = 0 
   while index < directors.length do 
-    directors_names = directors[index]
+    directors_name = directors[index]
     total += directors_earnings[directors]
     index += 1 
   end
